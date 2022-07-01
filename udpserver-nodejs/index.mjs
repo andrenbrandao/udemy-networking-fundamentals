87308c22ec7172543212384250013099e161b245
André Brandão <andrenbrandao@gmail.com>
2022-07-01 12:34:32 -0300

    feat: create udp servers with nodejs and go

---
+++ udpserver-nodejs/index.mjs
@@ -0,0 +1,8 @@
+import dgram from 'dgram';
+
+const socket = dgram.createSocket("udp4")
+socket.bind(5500, "127.0.0.1")
+socket.on("message", (msg, info) => {
+  console.log(`My server got a datagram ${msg}, from: ${info.address}:${info.port}`)
+})
+
